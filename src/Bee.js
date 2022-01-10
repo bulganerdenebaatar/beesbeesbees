40529cd23acf137d915076685bc4f6726e1c4ca3
Bulgan Erdenebaatar <bulganebaatar@gmail.com>
2022-01-10 11:00:42 -0800

    assignment complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
 var Bee = function() {
+  Grub.call(this); // calling parent constructor
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
-
+Bee.prototype = Object.create(Grub.prototype); //inherit from superclass Grub (new Grub)
+Bee.prototype.constructor = Bee; //list Bee as the actual constructor
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
 var HoneyMakerBee = function() {
-};
+  Bee.call(this); // inherit from Bee
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 
+};
+HoneyMakerBee.prototype = Object.create(Grub.prototype); // inherit from Grub
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot += 1;
+};
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot -= 1;
+};
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
